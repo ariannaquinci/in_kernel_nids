e137cc812395040907fa530101996010d5dd7a44
ariannaquinci <arianna.quinci99@gmail.com>
2026-03-16 16:30:17 +0100

    removed unuseful stuff

--- phd/xdp/src/c/modules/deferred_work.h
+++ phd/xdp/src/c/modules/deferred_work.h
@@ -7,9 +7,6 @@
 #include <net/netfilter/nf_queue.h>
 #include "dw_shared.h"
 
-/* bypass per evitare loop su reinjection */
-bool dw_is_bypass_skb(struct sk_buff *skb);
-
 /* stato completamento */
 u32  dw_get_done_mask(u32 pkt_id);
 bool dw_are_done(u32 pkt_id, u32 req_mask, u32 *done_out);
@@ -18,8 +15,7 @@ bool dw_are_done(u32 pkt_id, u32 req_mask, u32 *done_out);
 int  dw_get_verdict(u32 pkt_id);
 void dw_note_payload_signature(u32 pkt_id, u32 req_mask, bool is_malicious);
 
-/* bufferizzazione (copia skb + queue interna) */
-int  dw_buffer_marked_skb(struct sk_buff *skb, u32 pkt_id, u32 req_mask, u32 mark_dummy);
+/* bufferizzazione via NFQUEUE */
 int  dw_buffer_nfqueue_entry(struct nf_queue_entry *entry, u32 pkt_id, u32 req_mask);
 
 /* correlazione XDP->skb */
